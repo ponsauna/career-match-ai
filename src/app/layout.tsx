4eee3c69c96640f9c0c9b563dac9f0cf0f32fcd0
城田剛 <shirotatsuyoshi@shirotatsuyoshis-MacBook-Air.local>
2025-04-19 10:28:52 +0900

    smartphone change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       {/* bg-gray-50 と text-gray-800 をここに */}
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-800">
         <Header />
-        <main className="flex-1 w-full max-w-screen-lg mx-auto px-24 py-24">
+        <main className="flex-1 w-full max-w-screen-lg mx-auto px-4 sm:px-8 md:px-24 py-8 sm:py-16 md:py-24">
           {children}
         </main>
         <footer className="bg-white shadow-inner py-4">
